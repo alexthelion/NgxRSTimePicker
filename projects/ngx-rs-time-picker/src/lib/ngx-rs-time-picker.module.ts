13380fe8e1ab472174e33a371c010c5412ff3e0d
shimanski <shimanski.alex@gmail.com>
2020-08-14 21:03:23 +0300

    fixed imports

--- projects/ngx-rs-time-picker/src/lib/ngx-rs-time-picker.module.ts
+++ projects/ngx-rs-time-picker/src/lib/ngx-rs-time-picker.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
-import { NgxRsTimePickerComponent } from './ngx-rs-time-picker.component';
+import {NgModule} from '@angular/core';
+import {NgxRsTimePickerComponent} from './ngx-rs-time-picker.component';
 import {MinutesDirective} from './minutes.directive';
 import {HoursDirective} from './hours.directive';
-import {BrowserModule} from '@angular/platform-browser';
 import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [NgxRsTimePickerComponent, HoursDirective, MinutesDirective],
   imports: [
-    CommonModule,
-    BrowserModule
+    CommonModule
   ],
   exports: [NgxRsTimePickerComponent]
 })
